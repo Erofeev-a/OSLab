e3835e0c1f5f4237f11b5a6e8c7b11ff899566a8
Erofeev-a <54417147+Erofeev-a@users.noreply.github.com>
2019-09-05 22:47:35 +0300

    Add files via upload

---
+++ week3/ex2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void bubble_sort(long list[], long n)
+{
+  long c, d, t;
+ 
+  for (c = 0 ; c < n - 1; c++)
+  {
+    for (d = 0 ; d < n - c - 1; d++)
+    {
+      if (list[d] > list[d+1])
+      {
+        t         = list[d];
+        list[d]   = list[d+1];
+        list[d+1] = t;
+      }
+    }
+  }
+} 
+
+int main()
+{
+  long array[100], n, c, d, swap;
+ 
+  printf("Number of elements\n");
+  scanf("%ld", &n);
+ 
+  printf("Enter %ld integers\n", n);
+ 
+  for (c = 0; c < n; c++)
+    scanf("%ld", &array[c]);
+ 
+  bubble_sort(array, n);
+ 
+  printf("Sorted array:\n");
+ 
+  for (c = 0; c < n; c++)
+     printf("%ld\n", array[c]);
+ 
+  return 0;
+}
+ 
